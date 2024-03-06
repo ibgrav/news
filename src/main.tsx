142092f1af43c284ad6b011e6452129aa64170f6
Isaac Graves <isaac.graves@thetradedesk.com>
2024-03-06 13:38:52 -0800

    remove top-level await

--- src/main.tsx
+++ src/main.tsx
@@ -6,27 +6,35 @@ import { Home } from "./home";
 import { HN } from "./types";
 import { Item } from "./item";
 
-let children: ReactNode = <h1>404</h1>;
+main();
 
-if (location.pathname === "/") {
-  const url = new URL(`https://hacker-news.firebaseio.com/v0/topstories.json`);
-  const res = await fetch(url);
+async function main() {
+  let children: ReactNode = <h1>404</h1>;
 
-  const ids = (await res.json()) as Array<number>;
+  if (location.pathname === "/") {
+    const url = new URL(
+      `https://hacker-news.firebaseio.com/v0/topstories.json`
+    );
+    const res = await fetch(url);
 
-  children = <Home ids={ids.splice(0, 100)} />;
+    const ids = (await res.json()) as Array<number>;
+
+    children = <Home ids={ids.splice(0, 100)} />;
+  }
+
+  if (location.pathname.startsWith("/item/")) {
+    const id = location.pathname.replace("/item/", "");
+    const url = new URL(
+      `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+    );
+
+    const res = await fetch(url);
+    const item = (await res.json()) as HN.Item;
+
+    children = <Item {...item} />;
+  }
+
+  createRoot(document.getElementById("root") as HTMLDivElement).render(
+    <StrictMode>{children}</StrictMode>
+  );
 }
-
-if (location.pathname.startsWith("/item/")) {
-  const id = location.pathname.replace("/item/", "");
-  const url = new URL(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
-
-  const res = await fetch(url);
-  const item = (await res.json()) as HN.Item;
-
-  children = <Item {...item} />;
-}
-
-createRoot(document.getElementById("root") as HTMLDivElement).render(
-  <StrictMode>{children}</StrictMode>
-);
